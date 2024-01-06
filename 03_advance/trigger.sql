a8f864a51d1dcbfcb85951e0ba6a4bd12e4531cc
AhsenRiaz <ahsenriaz2001@gmail.com>
2024-01-06 21:36:23 +0500

    feat: trigger

--- 03_advance/trigger.sql
+++ 03_advance/trigger.sql
@@ -1,10 +1,52 @@
-CREATE TABLE trigger_test(message VARCHAR(100));
 
-DELIMITER $$ 
-CREATE TRIGGER my_trigger BEFORE
-INSERT ON employee 
-FOR EACH ROW BEGIN
-INSERT INTO
-    trigger_test("a new employee added");
+-- the trigger table
+CREATE TABLE trigger_test(
+	message VARCHAR(100)
+);
 
-END $ $ DELIMITER;
+
+-- trigger to insert a message each time an employee gets inserted into employee table
+DELIMITER $$
+CREATE 
+	TRIGGER my_trigger BEFORE INSERT
+	ON employee
+	FOR EACH ROW BEGIN 
+		INSERT INTO trigger_test VALUES("a new employee added");
+	END$$
+DELIMITER ;
+
+-- the above trigger will run before inserting into employee each time we insert into employee
+INSERT INTO employee 
+VALUES(110, , 'Kevin' , 'Malone', '1978-02-19', 69000, 106, 3);
+
+
+
+-- same trigger but now we are grabbing the name of the inserted employee
+DELIMITER $$
+CREATE 
+	TRIGGER my_trigger BEFORE INSERT
+	ON employee
+	FOR EACH ROW BEGIN 
+		INSERT INTO trigger_test VALUES(NEW.first_name);
+	END$$
+DELIMITER ;
+
+INSERT INTO employee 
+VALUES(111, , 'Jason' , 'Mark', '1971-05-11', 57000, 104, 2);
+
+DELIMITER $$
+CREATE 
+	TRIGGER my_trigger BEFORE INSERT
+	ON employee
+	FOR EACH ROW BEGIN
+        IF NEW.sex = 'M' THEN
+		    INSERT INTO trigger_test VALUES('added male employee');
+        ELSEIF NEW.sex = 'F' THEN
+            INSERT INTO trigger_test VALUES('added female employee')
+	END IF;
+    END$$
+DELIMITER ;
+
+
+SELECT * FROM trigger_test;
+
